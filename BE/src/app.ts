736f0c2f91c45d3ed8a08019f35186f250dfded3
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2025-07-05 20:37:01 +0700

    time out if > 10s

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
-import { sqlLogger } from './middleware/sqlLogger';
-import pool from './config/db';
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { sqlLogger } from "./middleware/sqlLogger";
+import pool from "./config/db";
+import timeout from "connect-timeout";
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,15 +11,15 @@ const app = express();
 
 app.locals.pool = pool;
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://motionboard-app.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: false
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://motionboard-app.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false,
+  })
+);
+app.use(timeout("10s"));
 
 app.use(express.json());
 app.use(sqlLogger);
