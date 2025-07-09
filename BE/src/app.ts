612cec5ff0231269f3027f0b48da9e10bebd588f
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2025-07-09 10:25:57 +0700

    deploy ci-cd in be

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -24,7 +24,7 @@ app.use(
   })
 );
 
-app.use(timeout("20s"));
+app.use(timeout("18s"));
 
 app.use(express.json());
 app.use(sqlLogger);
--- FE/src/app/test/index.tsx
+++ FE/src/app/test/index.tsx
@@ -15,7 +15,7 @@ type Scenario = {
 const TestPage = () => {
   const dispatch = useDispatch<AppDispatch>()
 
-  // Upload single file with redux + progress
+  // Upload single file with redux + progress12
   const onUploadSingle = async (file: File, onProgress: (percent: number) => void) => {
     onProgress(30)
     const formData = new FormData()
